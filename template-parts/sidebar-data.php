0e345bc8d1db2a08784430fd1394ace215a55353
FarizPe <fariz.perdana212@gmail.com>
2021-11-12 13:40:01 +0700

    first commit

---
+++ template-parts/sidebar-data.php
@@ -0,0 +1,8 @@
+<aside class="sidebar">
+	<div class="widget">
+		<ul>
+			<li><a href="list-data.php"><span class="fa fa-bars"></span> List Data</a></li>
+			<li><a href="tambah-data.php"><span class="fa fa-plus"></span> Tambah Data</a></li>
+		</ul>
+	</div>
+</aside>
